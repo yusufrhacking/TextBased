2afef4098347bc50973bf5dcf71d1e6f3aed26ae
hackingy <yusufhacking@gmail.com>
2023-03-01 08:45:33 -0500

    Defined headers for Component and System managers

--- src/ECS/Design/Managers/ComponentManager.h
+++ src/ECS/Design/Managers/ComponentManager.h
@@ -2,8 +2,23 @@
 #define TEXTBASED_COMPONENTMANAGER_H
 
 
-class ComponentManager {
+#include "../Objects/Entity.h"
 
+class ComponentManager {
+private:
+
+public:
+    template <typename TComponent, typename ...TArgs>
+    void addComponentToEntity(Entity entity, TArgs&& ... args);
+
+    template <typename T>
+    void removeComponent(Entity entity);
+
+    template <typename T>
+    bool hasComponent(Entity entity);
+
+    template <typename TComponent>
+    TComponent& getComponent(Entity entity) const;
 };
 
 
--- src/ECS/Design/Managers/SystemManager.h
+++ src/ECS/Design/Managers/SystemManager.h
@@ -2,8 +2,23 @@
 #define TEXTBASED_SYSTEMMANAGER_H
 
 
+#include "../Objects/Entity.h"
+
 class SystemManager {
 
+    template <typename TSystem, typename ... TArgs>
+    void addSystem(TArgs&& ... args);
+
+    template <typename TSystem>
+    void removeSystem();
+
+    template <typename TSystem>
+    bool hasSystem() const;
+
+    template <typename TSystem>
+    TSystem& getSystem() const;
+
+    void addEntityToSystems(Entity entity);
 };
 
 
