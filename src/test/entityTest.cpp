1e8d73351e91c4f9723296239701af2c54732d4c
hackingy <yusufhacking@gmail.com>
2023-04-19 00:13:06 -0400

    trying to use ECSmanager in test file, running into issues

---
+++ src/test/entityTest.cpp
@@ -0,0 +1,10 @@
+#include <catch2/catch_test_macros.hpp>
+#include "../main/ECS/Design/Managers/ECSManager.h"
+
+
+TEST_CASE("Add and retrieve entity?"){
+    std::unique_ptr<ECSManager> ecsManager = std::make_unique<ECSManager>();
+//    Entity testEntity = ecsManager->createEntity();
+//    ecsManager->addComponentToEntity<StyleComponent>(testEntity);
+//    REQUIRE(typeid(ecsManager->getComponent<StyleComponent>(testEntity)) == typeid(StyleComponent));
+}
--- src/test/test.cpp
+++
@@ -1,3 +0,0 @@
-//
-// Created by Yusuf Hacking on 4/18/23.
-//
