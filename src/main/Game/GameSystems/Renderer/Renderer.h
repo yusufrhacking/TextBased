836dfd19d1234a8e1f2081a1b06f8f9ff8ab2ad5
hackingy <yusufhacking@gmail.com>
2023-04-28 17:03:34 -0400

    Adding a GMockRenderer!

--- src/main/Game/GameSystems/Renderer/Renderer.h
+++ src/main/Game/GameSystems/Renderer/Renderer.h
@@ -1,5 +1,6 @@
 #ifndef TEXTBASED_RENDERER_H
 #define TEXTBASED_RENDERER_H
+#include <gmock/gmock.h>
 
 
 #include "../../../ECS/Components/TextComponent.h"
@@ -11,6 +12,12 @@ class Renderer {
         virtual void renderText(const std::shared_ptr<Position>& position, const TextComponent& sprite, const StyleComponent& style) = 0;
         virtual void renderFrame() = 0;
 
+};
+
+class MockRenderer : public Renderer {
+public:
+    MOCK_METHOD(void, renderText, (const std::shared_ptr<Position>& position, const TextComponent& sprite, const StyleComponent& style), (override));
+    MOCK_METHOD(void, renderFrame, (), (override));
 };
 
 
