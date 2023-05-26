31f3a7857324c0ecf6b85ebeb7ea78f0c934b019
hackingy <yusufhacking@gmail.com>
2023-05-26 15:08:40 -0500

    Basic logging is not broken --> I don't remember if this type of usage was broken previously

--- src/main/Main.cpp
+++ src/main/Main.cpp
@@ -1,4 +1,5 @@
 #include "Game/Game.h"
+#include "spdlog/spdlog.h"
 
 std::unique_ptr<ECSManager> ecsManager;
 std::unique_ptr<EventBus> eventBus;
@@ -7,6 +8,7 @@ std::unique_ptr<Window> window;
 int main() {
     ecsManager = std::make_unique<ECSManager>();
     eventBus = std::make_unique<EventBus>();
+    spdlog::info("Hello, {}!", "World");
 
     Game* game = new Game();
 
