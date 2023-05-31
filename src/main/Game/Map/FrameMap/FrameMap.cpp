b42c23313010adb113db4ef5cb26ad5008f09086
hackingy <yusufhacking@gmail.com>
2023-05-30 20:00:38 -0500

    Some parts of the logic created for the frame map, I think I need to write some tests though

--- src/main/Game/Map/FrameMap/FrameMap.cpp
+++ src/main/Game/Map/FrameMap/FrameMap.cpp
@@ -1,1 +1,24 @@
 #include "FrameMap.h"
+
+FrameMap::FrameMap(Position startingPosition) {
+    this->startingPosition = startingPosition;
+    //create frame at this position
+}
+
+Frame &FrameMap::getFrame(Position position) {
+    int xIndex = getXIndexFromPosition(position.xPos);
+    int yIndex = getYIndexFromPosition(position.yPos);
+    return frameMap[xIndex][yIndex];
+}
+
+void FrameMap::surroundLocation(Position playerPosition) {
+
+}
+
+int FrameMap::getXIndexFromPosition(float xPos) {
+    return 0;
+}
+
+int FrameMap::getYIndexFromPosition(float yPos) {
+    return 0;
+}
--- src/main/Game/Map/FrameMap/FrameMap.h
+++ src/main/Game/Map/FrameMap/FrameMap.h
@@ -2,16 +2,23 @@
 #define TEXTBASED_FRAMEMAP_H
 #include "../../../Helpers/Objects/Position.h"
 #include "../Frame/Frame.h"
+#include "../../GameSystems/Window/Window.h"
+#include <vector>
+
 
 class FrameMap {
     //Need to make it that frames can only be created here, so that I can track it that way
     //Perhaps store all the frames in this folder, and then make it protected so nobody else can call the constructors?
     //Will interact a lot with the MapGenerationSystem
 private:
+    int frameWidth = Window::windowWidth;
+    int frameHeight = Window::windowHeight;
+    std::vector<std::vector<Frame>> frameMap;
+    Position startingPosition;
 
 public:
     //Will create the initial frame, and perhaps surrounding frames (or leave this to what is called in the update)
-    void setup(Position startingPosition);
+    explicit FrameMap(Position startingPosition);
 
     Frame& getFrame(Position position);
 
@@ -19,6 +26,9 @@ public:
 
     //void surroundLocation(Position playerLocation); --> this is called in the MapGenerationSystem, like every frame
     //Will seed the 8 locations
+    int getXIndexFromPosition(float xPos);
+
+    int getYIndexFromPosition(float yPos);
 };
 
 
