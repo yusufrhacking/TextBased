d54cf9fd3d2020f521300294e11bc89cf4c8b4ea
hackingy <yusufhacking@gmail.com>
2023-05-31 15:18:48 -0500

    Cleaned up the sections so it runs faster

--- src/tests/Game/FrameMapTest.cpp
+++ src/tests/Game/FrameMapTest.cpp
@@ -10,37 +10,37 @@ TEST_CASE("Frame Map Surround Location Test", "[FrameMap]"){
     FrameMap frameMap(startingPosition);
 
     frameMap.surroundLocation(startingPosition);
-    SECTION("Top Right Is Created"){
+//    SECTION("Top Right Is Created"){
         Position topRight = startingPosition + Position((float)Window::windowWidth, -(float)Window::windowHeight);
         REQUIRE(frameMap.isFrameAtPositionFilled(topRight));
-    }
-    SECTION("Top Middle Is Created"){
+//    }
+//    SECTION("Top Middle Is Created"){
         Position topMiddle = startingPosition + Position(0, -(float)Window::windowHeight);
         REQUIRE(frameMap.isFrameAtPositionFilled(topMiddle));
-    }
-    SECTION("Top Left Is Created"){
+//    }
+//    SECTION("Top Left Is Created"){
         Position topLeft = startingPosition + Position(-(float)Window::windowWidth, -(float)Window::windowHeight);
         REQUIRE(frameMap.isFrameAtPositionFilled(topLeft));
-    }
-    SECTION("Middle Left Is Created"){
+//    }
+//    SECTION("Middle Left Is Created"){
         Position middleLeft = startingPosition + Position(-(float)Window::windowWidth, 0);
         REQUIRE(frameMap.isFrameAtPositionFilled(middleLeft));
-    }
-    SECTION("Middle Right Is Created"){
+//    }
+//    SECTION("Middle Right Is Created"){
         Position middleRight = startingPosition + Position((float)Window::windowWidth, 0);
         REQUIRE(frameMap.isFrameAtPositionFilled(middleRight));
-    }
-    SECTION("Bottom Right Is Created"){
+//    }
+//    SECTION("Bottom Right Is Created"){
         Position bottomRight = startingPosition + Position((float)Window::windowWidth, (float)Window::windowHeight);
         REQUIRE(frameMap.isFrameAtPositionFilled(bottomRight));
-    }
-    SECTION("Bottom Middle Is Created"){
+//    }
+//    SECTION("Bottom Middle Is Created"){
         Position bottomMiddle = startingPosition + Position(0, (float)Window::windowHeight);
         REQUIRE(frameMap.isFrameAtPositionFilled(bottomMiddle));
-    }
-    SECTION("Bottom Left Is Created"){
+//    }
+//    SECTION("Bottom Left Is Created"){
         Position bottomLeft = startingPosition + Position(-(float)Window::windowWidth, (float)Window::windowHeight);
         REQUIRE(frameMap.isFrameAtPositionFilled(bottomLeft));
-    }
+//    }
 
 }
