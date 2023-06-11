16dd635970652a68530ff67aaf308185b47f31d4
hackingy <yusufhacking@gmail.com>
2023-06-10 20:35:31 -0500

    More tidying, next step is to clean up each frame a little bit

--- src/main/ECS/Systems/SpecialSystems/MapGenerationSystem.cpp
+++ src/main/ECS/Systems/SpecialSystems/MapGenerationSystem.cpp
@@ -13,10 +13,6 @@ MapGenerationSystem::MapGenerationSystem(std::shared_ptr<FrameMap> frameMap) {
     this->frameMap = frameMap;
     requireComponent<PositionComponent>();
     requireComponent<MainPlayerComponent>();
-
-    Entity entity = *getRelevantEntities().begin();
-    auto playerPosition = ecsManager->getComponentFromEntity<PositionComponent>(entity).getPosition();
-    frameMap->surroundLocation(playerPosition);
 }
 
 void MapGenerationSystem::update() {
@@ -26,15 +22,5 @@ void MapGenerationSystem::update() {
     Entity entity = *getRelevantEntities().begin();
     auto playerPosition = ecsManager->getComponentFromEntity<PositionComponent>(entity).getPosition();
 
-//    frameMap->surroundLocation(playerPosition);
-
-    //Idea, when I get to the middle of the frame, I need to fill in the 8 frames around me
-    //Do this forever
-    //To do this, I will need a map object that can check if frames are "generated"
-    //And then also;
-//    if(currFrame > 0 && !doneBefore){
-//        spdlog::debug("Forest Frame inputted");
-//        ForestFrame give_me_a_name(camera->getCameraPosition());
-//        doneBefore = true;
-//    }
+    frameMap->surroundLocation(playerPosition);
 }
