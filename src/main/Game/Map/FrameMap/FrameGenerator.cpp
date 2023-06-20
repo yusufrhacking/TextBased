902569943647bf95ef462002b57953c65b720197
hackingy <yusufhacking@gmail.com>
2023-06-19 22:11:26 -0400

    Moved openPathsSignature to the FrameGenerator class

--- src/main/Game/Map/FrameMap/FrameGenerator.cpp
+++ src/main/Game/Map/FrameMap/FrameGenerator.cpp
@@ -17,8 +17,25 @@ void FrameGenerator::generateFrame(MapPosition nextFrameMapPosition) {
     }
 }
 
+std::bitset<4> FrameGenerator::getOpenPathsSignature(FrameCell cell){
+    auto neighbors = frameMap->getNeighborsOf(cell.mapPosition);
+    auto openSidesSignature = std::bitset<4>();
+    for (int x = 0; x < neighbors.size(); x++){//0 = N, 1 = E, 2 = S, 3 = W
+        if (!neighbors[x]->isFilled){
+            openSidesSignature.set(x, true);
+            continue;
+        }
+        auto neighborIsThisWay = static_cast<Direction>(x);
+        auto relevantNeighborSide = FrameCell::getOppositeDirection(neighborIsThisWay);
+        if (neighbors[x]->isOpenAt(relevantNeighborSide)){
+            openSidesSignature.set(x, true);
+        }
+    }
+    return openSidesSignature;//TIME TO WRITE UNIT TESTS (SHOULD HAVE DONE THIS FIRST BUT I AM SLEEP DEPRIVED);
+}
+
 void FrameGenerator::frameCellAtPosition(FrameCell &newFrameCell) {
-//    auto openPathsSignature = newFrameCell.getOpenPathsSignature();
+    auto openPathsSignature = getOpenPathsSignature(newFrameCell);
     if (newFrameCell.biome == Biome::FOREST){
         if (rand() % 2 == 0){
             newFrameCell.frame = std::make_unique<VerticalForestFrame>(newFrameCell.gameReferencePosition);
