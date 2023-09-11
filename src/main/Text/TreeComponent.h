216a15f785d461a33d99c913239780f7f6088f31
hackingy <yusufhacking@gmail.com>
2023-09-11 17:13:41 -0400

    Cleaned up tree component, now it can be used in conjunction with the Axe component

--- src/main/Text/TreeComponent.h
+++ src/main/Text/TreeComponent.h
@@ -4,11 +4,8 @@
 #include "TextComponent.h"
 #include "TextGenerator.h"
 
-struct TreeComponent: public TextComponent{
-    TreeComponent(){
-        text = TextGenerator::getTreeText();
-        surfaceSize = {static_cast<RenderedVal>(getTextWidth() * MONACO_RENDERED_TEXT_WIDTH_SCALER), MONACO_HEIGHT_OF_A_LINE_OF_TEXT * getTextHeight()};
-    }
+struct TreeComponent{
+    TreeComponent() = default;
 };
 
 #endif
