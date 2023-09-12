4ff9d65183d6834c055dcbd9298ba538b9c0d4ca
hackingy <yusufhacking@gmail.com>
2023-09-12 17:02:59 -0400

    Cleaned up the collision component

--- src/main/PositionsAndMovement/CollisionComponent.h
+++ src/main/PositionsAndMovement/CollisionComponent.h
@@ -3,55 +3,7 @@
 #include "Size.h"
 
 struct CollisionComponent{
-    Size collisionRange{};
-
-    CollisionComponent(){
-        collisionRange = {0, 0};
-    }
-
-    explicit CollisionComponent(Size surfaceSize){
-        collisionRange = {surfaceSize.width, surfaceSize.height};
-    }
+    CollisionComponent() = default;
 };
-
-//struct CollisionComponent{
-//    RenderedVal widthCollisionRange;
-//    RenderedVal heightCollisionRange;
-//
-//    explicit CollisionComponent(const std::string& text){
-//        Size surfaceSize = {static_cast<RenderedVal>
-//                            (TextComponent::getTextWidth(text) * MONACO_RENDERED_TEXT_WIDTH_SCALER),
-//                            MONACO_HEIGHT_OF_A_LINE_OF_TEXT * TextComponent::getTextHeight(text)};
-//        this->widthCollisionRange = surfaceSize.width;
-//        this->heightCollisionRange = surfaceSize.height;
-//    }
-//
-//    explicit CollisionComponent(Size surfaceSize){
-//        this->widthCollisionRange = surfaceSize.width;
-//        this->heightCollisionRange = surfaceSize.height;
-//    }
-//
-//    explicit CollisionComponent(int width = 0, int height = 0){
-//        this->widthCollisionRange = width;
-//        this->heightCollisionRange = height;
-//    }
-//};
-
-/*
- * struct CollisionComponent{
-    Size collisionRange{};
-
-    explicit CollisionComponent(const std::string& text){
-        Size surfaceSize = {static_cast<RenderedVal>
-                            (TextComponent::getTextWidth(text) * MONACO_RENDERED_TEXT_WIDTH_SCALER),
-                            MONACO_HEIGHT_OF_A_LINE_OF_TEXT * TextComponent::getTextHeight(text)};
-        collisionRange = {surfaceSize.width, surfaceSize.height};
-    }
-
-    explicit CollisionComponent(Size surfaceSize){
-        collisionRange = {surfaceSize.width, surfaceSize.height};
-    }
-};
- */
 
 #endif
