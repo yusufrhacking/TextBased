2a33df5a4b6cc4aa9a0cf0231d18110d0a2b1b6b
hackingy <yusufhacking@gmail.com>
2023-09-13 13:57:42 -0400

    Added basic components for the wood

---
+++ src/main/Woodworking/PickupComponent.h
@@ -0,0 +1,8 @@
+#ifndef TEXTBASED_PICKUPCOMPONENT_H
+#define TEXTBASED_PICKUPCOMPONENT_H
+
+struct PickupComponent{
+    PickupComponent() = default;
+};
+
+#endif //TEXTBASED_PICKUPCOMPONENT_H
---
+++ src/main/Woodworking/WoodComponent.h
@@ -0,0 +1,8 @@
+#ifndef TEXTBASED_WOODCOMPONENT_H
+#define TEXTBASED_WOODCOMPONENT_H
+
+struct WoodComponent{
+    WoodComponent() = default;
+};
+
+#endif //TEXTBASED_WOODCOMPONENT_H
