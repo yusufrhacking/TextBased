bb108f0a510116c261710a03e4c581f95a8baeb5
Jaeson Martin <mail2jaeson@gmail.com>
2023-09-14 02:07:04 -0400

    created more stubbing this stuff doesn't work still though, look at lines 43-46

--- src/main/Creation/ItemCreationSystem.cpp
+++ src/main/Creation/ItemCreationSystem.cpp
@@ -40,6 +40,10 @@ void ItemCreationSystem::onStash(StashPlayerItemEvent& event) {
 }
 
 void ItemCreationSystem::stashAxe() {
+    auto mainPlayer = *getRelevantEntities().begin();
+    auto tiedChild = ecsManager->getComponentFromEntity<TiedChildComponent>(mainPlayer);
+//    auto axe = ecsManager->getComponentFromEntity<AxeComponent>(tiedChild);
+//    ecsManager->removeComponentFromEntity<TextComponent>(axe); //Need to figure out how to get the text component of the axe from player
 
 }
 
