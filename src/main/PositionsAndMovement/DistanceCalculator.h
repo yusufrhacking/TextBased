931dbbfaf8163adeed41134157e4cdd2190b490b
hackingy <yusufhacking@gmail.com>
2023-09-13 21:07:01 -0400

    Cleaned up the distance calculator a bit, still have weird float/int conversion stuff but works fine

--- src/main/PositionsAndMovement/DistanceCalculator.h
+++ src/main/PositionsAndMovement/DistanceCalculator.h
@@ -8,55 +8,29 @@
 class DistanceCalculator {
 public:
     static bool isInAllowedRange(Position pos1, Position pos2, const Size& size1, const Size& size2, float allowedDistance) {
-        auto endpointLeftBound = pos2.xPos;
-        auto endpointRightBound = pos2.xPos + size2.width;
-        auto endpointTopBound = pos2.yPos;
-        auto endpointBottomBound = pos2.yPos + size2.height;
+        float horizontalDistance = calculateSideDistance(pos1.xPos, pos1.xPos + size1.width, pos2.xPos, pos2.xPos + size2.width);
+        float verticalDistance = calculateSideDistance(pos1.yPos, pos1.yPos + size1.height, pos2.yPos, pos2.yPos + size2.height);
 
-        auto startLeftBound = pos1.xPos;
-        auto startRightBound = pos1.xPos + size1.width;
-        auto startTopBound = pos1.yPos;
-        auto startBottomBound = pos1.yPos + size1.height;
-
-        float horizontalDistance = 0.0f;
-        if (startRightBound < endpointLeftBound) {
-            horizontalDistance = endpointLeftBound - startRightBound;
-        } else if (startLeftBound > endpointRightBound) {
-            horizontalDistance = startLeftBound - endpointRightBound;
-        }
-
-        float verticalDistance = 0.0f;
-        if (startBottomBound < endpointTopBound) {
-            verticalDistance = endpointTopBound - startBottomBound;
-        } else if (startTopBound > endpointBottomBound) {
-            verticalDistance = startTopBound - endpointBottomBound;
-        }
-
-        float shortestDistance = std::sqrt(horizontalDistance * horizontalDistance + verticalDistance * verticalDistance);
+        float shortestDistance = calculateEuclideanDistance(horizontalDistance, verticalDistance);
 
         return shortestDistance <= allowedDistance;
     }
 
 private:
-    static bool isWithinBounds(Position mainPos, Position otherPos, const Size& otherSize) {
-        bool isWithinHorizontalBounds = mainPos.xPos >= otherPos.xPos && mainPos.xPos <= otherPos.xPos + otherSize.width;
-        bool isWithinVerticalBounds = mainPos.yPos >= otherPos.yPos && mainPos.yPos <= otherPos.yPos + otherSize.height;
-
-        return isWithinHorizontalBounds && isWithinVerticalBounds;
+    static float calculateSideDistance(float entityBegin, float entityEnd, float otherBegin, float otherEnd) {
+        if (entityEnd < otherBegin) {
+            return otherBegin - entityEnd;
+        } else if (entityBegin > otherEnd) {
+            return entityBegin - otherEnd;
+        }
+        return 0.0f;
     }
 
-    static Position getClosestPart(Position mainPos, Position otherPos, const Size& otherSize) {
-        float closestX = std::clamp(mainPos.xPos, otherPos.xPos, otherPos.xPos + otherSize.width);
-        float closestY = std::clamp(mainPos.yPos, otherPos.yPos, otherPos.yPos + otherSize.height);
-
-        return {closestX, closestY};
-    }
-
-    static bool isWithinAllowedDistance(Position mainPos, Position point, float allowedDistance) {
-        float distanceSquared = std::pow(point.xPos - mainPos.xPos, 2) + std::pow(point.yPos - mainPos.yPos, 2);
-        return distanceSquared <= std::pow(allowedDistance, 2);
+    static float calculateEuclideanDistance(float x, float y) {
+        return std::sqrt(x * x + y * y);
     }
 };
+
 
 
 #endif //TEXTBASED_DISTANCECALCULATOR_H
