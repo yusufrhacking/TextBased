4c6cad2e070aa636d26b0cbe4233c53c703ca1b6
hackingy <yusufhacking@gmail.com>
2023-09-19 10:12:06 -0400

    Spend word function implemented now

--- src/main/Lettering/CharacterStorageSystem.cpp
+++ src/main/Lettering/CharacterStorageSystem.cpp
@@ -6,9 +6,17 @@ CharacterStorageSystem::CharacterStorageSystem() = default;
 bool CharacterStorageSystem::spendWord(std::string word) {
     Alphabet dummyAlphabet = alphabet;
     for (char c : word){
-
+        auto character = char_to_enum(c);
+        if (dummyAlphabet.getCount(character) <= 0){
+            return false;
+        }
+        dummyAlphabet.decrement(character);
     }
-    return false;
+    for (char c : word){
+        auto character = char_to_enum(c);
+        alphabet.decrement(character);
+    }
+    return true;
 }
 
 const Alphabet &CharacterStorageSystem::getAlphabet() {
