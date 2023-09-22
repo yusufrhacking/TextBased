3598d9e9ebd2c7e1afed38f3044b09928b8dbf03
hackingy <yusufhacking@gmail.com>
2023-09-22 19:34:49 -0400

    CommandLog has an interface ish now

--- src/main/TextCommands/CommandLog.cpp
+++ src/main/TextCommands/CommandLog.cpp
@@ -1,1 +1,11 @@
 #include "CommandLog.h"
+
+CommandLog::CommandLog() = default;
+
+void CommandLog::addCommand(const Command& command) {
+    commands.push_back(command);
+}
+
+std::vector<Command> CommandLog::getCommands() const {
+    return commands;
+}
--- src/main/TextCommands/CommandLog.h
+++ src/main/TextCommands/CommandLog.h
@@ -1,9 +1,15 @@
 #ifndef TEXTBASED_COMMANDLOG_H
 #define TEXTBASED_COMMANDLOG_H
 
-
+#include "Command.h"
 class CommandLog {
+    std::vector<Command> commands;
+public:
+    CommandLog();
 
+    void addCommand(const Command &command);
+
+    std::vector<Command> getCommands() const;
 };
 
 
