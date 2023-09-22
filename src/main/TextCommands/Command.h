09550eecd3b2ab9a9ee31efb2798ce697139fc60
hackingy <yusufhacking@gmail.com>
2023-09-22 19:26:33 -0400

    Command data structure kinda exists now

--- src/main/TextCommands/Command.h
+++ src/main/TextCommands/Command.h
@@ -2,14 +2,18 @@
 #define TEXTBASED_COMMAND_H
 
 #include <string>
+#include <utility>
 
 enum class Author{
-
+    PLAYER,
+    ENGINEER,
+    BRICOLEUR
 };
 
 struct Command{
     Author author;
     std::string command;
+    Command(Author author, std::string command): author(author), command(std::move(command)){}
 };
 
 #endif
