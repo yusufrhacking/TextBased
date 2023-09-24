c631b666611b9c2306f64dc1eee9dc8a2acc13dd
hackingy <yusufhacking@gmail.com>
2023-09-24 19:56:31 -0400

    LetterBank characters show up a little bit now

--- src/main/Lettering/LetterBankRenderSystem.cpp
+++ src/main/Lettering/LetterBankRenderSystem.cpp
@@ -7,6 +7,22 @@ extern std::unique_ptr<ECSManager> ecsManager;
 
 void LetterBankRenderSystem::render(const std::shared_ptr<Renderer> &renderer, Camera camera) {
     renderLetterBox(renderer);
+    auto allLetters = Alphabet::allLetters();
+    int xCount = 1;
+    int yCount = 1;
+    int rowReset = 7;
+    for (auto letter: allLetters){
+        auto submittedPosition = letterBoxPosition + Position(LETTER_X_OFFSET*xCount, LETTER_Y_OFFSET*yCount);
+        std::string letterStr;
+        letterStr.push_back(enum_to_char(letter));
+        renderer->renderText(unusedCamera, submittedPosition,
+                             TextComponent(letterStr), StyleComponent(Style::LETTER));
+        xCount++;
+        if (xCount > rowReset){
+            xCount = 1;
+            yCount += 1;
+        }
+    }
     auto letterAllocations = ecsManager->getSystem<CharacterStorageSystem>().getAlphabet();
 }
 
