6c7fe93292485c662f9bc26a22e9804d9c9ee45e
hackingy <yusufhacking@gmail.com>
2023-09-24 19:40:13 -0400

    Letter bank kinda spaced properly and allat

--- src/main/Lettering/LetterBankRenderSystem.h
+++ src/main/Lettering/LetterBankRenderSystem.h
@@ -5,8 +5,17 @@
 class LetterBankRenderSystem: public RenderSystem {
 private:
     Camera unusedCamera{Position(0, 0)};
-    std::string letterBankText = "$$$$$$$$$$\n$        $\n$        $\n$        $\n$        $\n$$$$$$$$$$";
-    Position letterBoxPosition = Window::getTopRightPosition() - Position(150, 0);
+    std::string letterBankText = "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$\n"
+                                 "$                                    $\n"
+                                 "$                                    $\n"
+                                 "$                                    $\n"
+                                 "$                                    $\n"
+                                 "$                                    $\n"
+                                 "$                                    $\n"
+                                 "$                                    $\n"
+                                 "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$";
+    Position letterBoxPosition = Window::getTopRightPosition() -
+            Position(TextComponent::getTextWidth(letterBankText)*MONACO_RENDERED_TEXT_WIDTH_SCALER, 0);
 public:
     LetterBankRenderSystem() = default;
     void render(const std::shared_ptr<Renderer>& renderer, Camera camera) override;
