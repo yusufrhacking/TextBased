330ebcb957d575efe256b2e612183652bdf8a8fb
hackingy <yusufhacking@gmail.com>
2023-09-27 20:42:22 -0400

    Adding basic health and death components that will be used

---
+++ src/main/Health/HealthComponent.h
@@ -0,0 +1,8 @@
+#ifndef TEXTBASED_HEALTHCOMPONENT_H
+#define TEXTBASED_HEALTHCOMPONENT_H
+
+struct HealthComponent{
+    HealthComponent() = default;
+};
+
+#endif //TEXTBASED_HEALTHCOMPONENT_H
---
+++ src/main/Health/PendingDeathComponent.h
@@ -0,0 +1,8 @@
+#ifndef TEXTBASED_PENDINGDEATHCOMPONENT_H
+#define TEXTBASED_PENDINGDEATHCOMPONENT_H
+
+struct PendingDeathComponent{
+    PendingDeathComponent() = default;
+};
+
+#endif //TEXTBASED_PENDINGDEATHCOMPONENT_H
