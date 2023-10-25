9b17a24bcf62ab324c448ae4f6386107d958d237
hackingy <yusufhacking@gmail.com>
2023-10-25 00:10:18 -0400

    Tree shows up next to the player now

--- src/main/Levels/IntroPac.cpp
+++ src/main/Levels/IntroPac.cpp
@@ -5,6 +5,7 @@
 #include "../PositionsAndMovement/LiveComponent.h"
 #include "../PositionsAndMovement/CollisionComponent.h"
 #include "../Health/HealthComponent.h"
+#include "../Woodworking/TreePrefab.h"
 #include <memory>
 extern std::unique_ptr<ECSManager> ecsManager;
 extern std::unique_ptr<EventBus> eventBus;
@@ -12,6 +13,7 @@ extern std::unique_ptr<EventBus> eventBus;
 IntroPac::IntroPac(Position startingPosition): startingPosition(startingPosition) {
     witt = ecsManager->createEntity();
     createPlayer();
+    TreePrefab tree{startingPosition + Position(100, -80)};
 }
 
 void IntroPac::createPlayer() {
