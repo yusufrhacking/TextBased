a72f5ec729fc6c0a8c7d7d8f35dc940fe4949c15
hackingy <yusufhacking@gmail.com>
2023-12-25 00:13:27 -0600

    I fixed the issue with St. Theresa's name not spacing properly

--- src/main/Middlemarch/NovelTextRenderSystem.cpp
+++ src/main/Middlemarch/NovelTextRenderSystem.cpp
@@ -121,6 +121,7 @@ void NovelTextRenderSystem::convertTextToEntities(Entity entity, PositionCompone
             ecsManager->getComponentFromEntity<TextComponent>(wordEntity).text = subject;
             ecsManager->addComponentToEntity<MainPlayerComponent>(wordEntity, std::make_shared<Velocity>(MONACO_RENDERED_TEXT_WIDTH_SCALER, MONACO_HEIGHT_OF_A_LINE_OF_TEXT));
             i += Split::getWordsAndPunctuation(subject).size();
+            word = subject;
         }
 
         if (i < words.size()-2) {
