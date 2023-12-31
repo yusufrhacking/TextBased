451a74377503ea7bbd39cbe05fe0e7490c9c07ef
hackingy <yusufhacking@gmail.com>
2023-12-30 20:54:21 -0600

    Finished basic function of system add system

--- src/main/HighLevel/SystemAddSystem.cpp
+++ src/main/HighLevel/SystemAddSystem.cpp
@@ -1,6 +1,17 @@
 #include "SystemAddSystem.h"
+
+#include "ECSManager.h"
 #include "../EventSystem/EventBus.h"
+#include "../Middlemarch/EndOfReadingEvent.h"
 
 extern std::unique_ptr<EventBus> eventBus;
+extern std::unique_ptr<ECSManager> ecsManager;
 
 
+SystemAddSystem::SystemAddSystem() {
+    eventBus->listenToEvent<EndOfReadingEvent>(this, &SystemAddSystem::onEndOfReading);
+}
+
+void SystemAddSystem::onEndOfReading(EndOfReadingEvent& event) {
+    ecsManager->addSystem<AbyzPrioritizingSystem>();
+}
--- src/main/HighLevel/SystemAddSystem.h
+++ src/main/HighLevel/SystemAddSystem.h
@@ -2,10 +2,14 @@
 #define SYSTEMADDSYSTEM_H
 
 #include "../ECSObjects/System.h"
+#include "../Middlemarch/EndOfReadingEvent.h"
 
 
 class SystemAddSystem: public System {
-
+public:
+    SystemAddSystem();
+private:
+    void onEndOfReading(EndOfReadingEvent& event);
 };
 
 
