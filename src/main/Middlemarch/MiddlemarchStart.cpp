f98e7f2948269489ede2810593505162cb5ca48a
hackingy <yusufhacking@gmail.com>
2024-01-03 01:12:17 -0600

    Avila now catches Saint Theresa

--- src/main/Middlemarch/MiddlemarchStart.cpp
+++ src/main/Middlemarch/MiddlemarchStart.cpp
@@ -25,15 +25,24 @@ extern std::unique_ptr<ECSManager> ecsManager;
 MiddlemarchStart::MiddlemarchStart(Position startingPosition): startPosition(startingPosition) {
     speakEngineer();
 
+    Position subjectPosition{11895, 10532};
+
     Entity subject = ecsManager->createEntity();
     ecsManager->addComponentToEntity<LiveComponent>(subject);
-    ecsManager->addComponentToEntity<PositionComponent>(subject, Position(11895, 10532));
+    ecsManager->addComponentToEntity<PositionComponent>(subject, subjectPosition);
     ecsManager->addComponentToEntity<TextComponent>(subject, "Saint Theresa");
     ecsManager->addComponentToEntity<MainPlayerComponent>(subject);
     ecsManager->addComponentToEntity<SubjectComponent>(subject);
     ecsManager->addComponentToEntity<GravityComponent>(subject);
     ecsManager->addComponentToEntity<VelocityComponent>(subject);
     ecsManager->addComponentToEntity<CollisionComponent>(subject);
+
+    Entity avila = ecsManager->createEntity();
+    ecsManager->addComponentToEntity<LiveComponent>(avila);
+    ecsManager->addComponentToEntity<PositionComponent>(avila, subjectPosition + Position(25, 800));
+    ecsManager->addComponentToEntity<TextComponent>(avila, "Avila");
+    ecsManager->addComponentToEntity<GenericStyleComponent>(avila);
+    ecsManager->addComponentToEntity<CollisionComponent>(avila);
 
     // createPreludeText();
 
