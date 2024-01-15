28acbc3c6b7b268a277fd0269f2a3b5c046a3398
hackingy <yusufhacking@gmail.com>
2024-01-14 22:14:47 -0600

    Create next step function now created

--- src/main/Platformer/PlatformGenerationSystem.cpp
+++ src/main/Platformer/PlatformGenerationSystem.cpp
@@ -3,23 +3,29 @@
 #include "TextStepPrefab.h"
 
 PlatformGenerationSystem::PlatformGenerationSystem(Position startPosition, std::vector<std::string> stepStrs) {
+    this->startPosition = startPosition;
     Position nextStepPos = startPosition;
     nextStepPos = nextStepPos - stepJump;
 
     for (const auto& nextStepStr : stepStrs) {
-        if(direction == -1) {
-            prevWordX = static_cast<float>(TextComponent::getSurfaceSize(nextStepStr).width);
-        }
-        nextStepPos.x += (stepJump.x + prevWordX) * direction;
-        nextStepPos.y += stepJump.y;
-        if (nextStepPos.x + TextComponent::getSurfaceSize(nextStepStr).width > Window::deriveRelativeBottomRight(startPosition).x) {
-            nextStepPos.x -= stepJump.x * 2;
-            nextStepPos.x -= prevWordX;
-            nextStepPos.x -= static_cast<float>(TextComponent::getSurfaceSize(nextStepStr).width);
-            direction = -1;
-        }
-        TextStepPrefab nextStepPrefab{nextStepStr, nextStepPos};
-        ecsManager->addComponentToEntity<GenericStyleComponent>(nextStepPrefab.entity);
+        createNextStep(nextStepPos, nextStepStr);
+    }
+}
+
+void PlatformGenerationSystem::createNextStep(Position& nextStepPos, const std::string& nextStepStr) {
+    Position startPosition;
+    if(direction == -1) {
         prevWordX = static_cast<float>(TextComponent::getSurfaceSize(nextStepStr).width);
     }
+    nextStepPos.x += (stepJump.x + prevWordX) * direction;
+    nextStepPos.y += stepJump.y;
+    if (nextStepPos.x + TextComponent::getSurfaceSize(nextStepStr).width > Window::deriveRelativeBottomRight(startPosition).x) {
+        nextStepPos.x -= stepJump.x * 2;
+        nextStepPos.x -= prevWordX;
+        nextStepPos.x -= static_cast<float>(TextComponent::getSurfaceSize(nextStepStr).width);
+        direction = -1;
+    }
+    TextStepPrefab nextStepPrefab{nextStepStr, nextStepPos};
+    ecsManager->addComponentToEntity<GenericStyleComponent>(nextStepPrefab.entity);
+    prevWordX = static_cast<float>(TextComponent::getSurfaceSize(nextStepStr).width);
 }
--- src/main/Platformer/PlatformGenerationSystem.h
+++ src/main/Platformer/PlatformGenerationSystem.h
@@ -4,10 +4,13 @@
 
 
 class PlatformGenerationSystem: public System {
+    Position startPosition{};
     Position stepJump{100, -50};
     float prevWordX = 0;
     float direction = 1;
+    void createNextStep(Position&nextStepPos, const std::string&nextStepStr);
 public:
+
     PlatformGenerationSystem(Position startPosition, std::vector<std::string> stepStrs);
 
 };
